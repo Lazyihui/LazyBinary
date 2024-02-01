0a83a4439020d827a450bab042551963d477331b
包子 <chengbaozi6@qq.com>
2024-02-01 22:54:31 +0800

    Sample

---
+++ Assets/Scprits/BinaryWriter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LazyBinary {
+
+    public static class BinaryWriter {
+        public static void Yo() {
+            Debug.Log("yo");
+        }
+    }
+
+}
